095f6be8800ae0ee3da96fad5033c47138f44477
MajorasBoi <melchorperezavila248@gmail.com>
2022-09-23 22:19:19 -0400

    Start

---
+++ Lab1/Tech_Store/Tech_Store/Class1.cs
@@ -0,0 +1,65 @@
+﻿namespace Tech_Store
+{
+    enum Brand
+    {
+        Samsung,
+        LG,
+        Apple,
+        Xiaomi,
+        Huawei,
+    }
+
+    enum MobileDataGen
+    {
+        _2G,
+        _3G,
+        LTE,
+    }
+
+    public abstract class Device
+    {
+        //Attributes
+        private Brand _brand { get; set; }
+        private float _price { get; set; }
+        private DateTime _year { get; set; }
+
+        //Methods
+        public abstract void ShowAllProps();
+    }
+
+    public class Mobile_Phone: Device
+    {
+        //Attributes
+        private MobileDataGen mobile_data_gen { get; set; }
+
+        //Methods
+        public override void ShowAllProps()
+        {
+            
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
