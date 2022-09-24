a7637a067522a5803222f638d560e6f72627ad9f
MajorasBoi <melchorperezavila248@gmail.com>
2022-09-24 15:01:59 -0400

    Exception handler implemented

--- Lab1/Tech_Store/ConsoleApp/Program.cs
+++ Lab1/Tech_Store/ConsoleApp/Program.cs
@@ -3,4 +3,19 @@
 DateTime d = DateTime.Now;
 Laptop l = new ((EBrand)1, 12, d, 23.5, (EGPU)0, (ECPU)0,(ELaptopStorage)0);
 l.ShowAllProps();
+System.Console.WriteLine("Introduzca la marca: ");
+
+try
+{
+
+    l.Brand = (EBrand)int.Parse(System.Console.ReadLine());
+
+}catch(FormatException e)
+{
+
+    System.Console.WriteLine($"Excepcion: {e.Message.ToString()}");
+
+}
+
+l.ShowAllProps();
 
